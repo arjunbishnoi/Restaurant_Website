34c2bf870e352365fa375146a7c4287cc845c3ef
Arjun Bishnoi <arjunbishnoi1@gmail.com>
2025-02-07 19:59:00 -0500

    commit

---
+++ models/order.js
@@ -0,0 +1,17 @@
+// REQUIRED: uses the mongoose library
+const mongoose = require("mongoose");
+
+
+// SCHEMA: data structure that represents ONE document in your collection
+// Specifies the properties of an item & the data types of each property
+const driverSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    vehicleModel: String,
+    licensePlate: String
+  }, { timestamps: true });
+
+// - If the collection does not exist, then create it
+// - Otherwise, use the existing collection
+module.exports = mongoose.model('Order', orderSchema);
--- server.js
+++ server.js
@@ -1,50 +1,90 @@
+// ------------------------- REQUIREMENTS --------------------------
+
+// Dependencies
 const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const mongoose = require('mongoose');
-require('dotenv').config();
 const port = process.env.PORT || 8080
+require('dotenv').config();
 
+// 'App' Defined
 const app = express()
 
+// Handlebar 
 app.engine('hbs', exphbs.engine({
-        extname: 'hbs',             // File extension
-        defaultLayout: false }));   // Disable default layout
-app.set('view engine', 'hbs');  // Set Handlebars as the template engine
-app.set('views', './views');    // Specify views directory
+    extname: 'hbs',
+    defaultLayout: false
+}));
+app.set('view engine', 'hbs');
 
-// tells express that this application will contain
-// endpoints that receives data from a form
+// URL Encoding
 app.use(express.urlencoded({ extended: true }))
 
-// Additional variables and definitions
+// Global Constants and Variables 
+const Driver = require("./models/driver.js");
+const Order = require("./models/driver.js");
 
 
-// HOME PAGE
+
+// ------------------------- HOME PAGE ----------------------------
+
 app.get("/", (req, res) => {
     return res.render("home.hbs")
 })
 
 
-// section 1 - Admin Page
-app.get("/admin", (req, res) => {
-    return res.render("./admin/admin.hbs")
-})
 
+// ----------------------- MENU SECTION ---------------------------
 
-// section 2 - Delivery Driver Page
-app.get("/deliver", (req, res) => {
-    return res.render("./deliver/deliver.hbs")
-})
-
-
-// section 3 - Menu Page
 app.get("/menu", (req, res) => {
     return res.render("./menu/menu.hbs")
 })
 
 
-// Starting Server
+
+// ----------------------- ADMIN SECTION --------------------------
+app.get("/admin", (req, res) => {
+    return res.render("./admin/admin.hbs")
+})
+
+
+
+// ---------------------- DRIVER SECTION --------------------------
+
+// Driver Homepage
+app.get("/driver", (req, res) => {
+    res.render("./driver/home.hbs");
+});
+
+// Driver Dashboard Page
+app.get("/driver/dashboard", (req, res) => {
+    res.render("./driver/dashboard.hbs");
+});
+
+// Driver Delivery Page
+app.get("/driver/deliver", (req, res) => {
+    res.render("./driver/deliver.hbs");
+});
+
+// Driver Login Page
+app.get("/driver/login", (req, res) => {
+    res.render("./driver/login.hbs");
+});
+
+// Driver Orders Page
+app.get("/driver/orders", (req, res) => {
+    res.render("./driver/orders.hbs");
+});
+
+// Driver Registration Page
+app.get("/driver/register", (req, res) => {
+    res.render("./driver/register.hbs");
+});
+
+
+
+// --------------------- SERVER AND MONGODB ------------------------
 const startServer = async () => {
     console.log(`The server is running on http://localhost:${port}`)
     console.log(`Press CTRL + C to exit`)
